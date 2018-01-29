033e9666b96255060935128f26dec1377e0e9899
Dan Yu <phossanx@gmail.com>
2018-01-28 22:51:18 -0500

    implementing program

--- src/beersong/BeerSong.java
+++ src/beersong/BeerSong.java
@@ -16,9 +16,19 @@ public class BeerSong {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Ninety_Nine_Bottles_of_Beer();
     }
     public static void Ninety_Nine_Bottles_of_Beer(){
-        
-        
+        int count;
+        for(count = 99; count > 1; count--){
+        System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer");
+        System.out.println("take one down, pass it around, " + (count-1) + " bottles of beer on the wall");
+    }
+        count=1;
+        if(count == 1){
+            System.out.println(count + " bottle of beer on the wall, " + count + " bottle of beer");
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall");
+        }
+           
     }
 }
